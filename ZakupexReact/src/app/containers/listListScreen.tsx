d3d3401914ccd1380617ba2e84a77f14569a1e71
jsofinski <kubsof@gmail.com>
2021-12-15 15:50:00 +0100

    Produktliste und Lisliste funktioniert

--- ZakupexReact/src/app/containers/listListScreen.tsx
+++ ZakupexReact/src/app/containers/listListScreen.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, Text, TouchableHighlight, SafeAreaView, FlatList, Button } from 'react-native';
+import { Alert, SafeAreaView, TextInput, View, Text, TouchableOpacity, TouchableHighlight, Modal, FlatList, Pressable, Button } from 'react-native';
 import styles from '../styles/style'
 import { Icon } from 'react-native-elements'
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { useNavigation } from '@react-navigation/native';
-import { newList, removeList } from '../redux/listReducer';
+import { newList, removeList, renameList } from '../redux/listReducer';
 import { ListOptions } from '../utilities/listHandler';
 
 
@@ -18,26 +18,159 @@ const exampleListOptions : ListOptions= {
 export default function ListListScreen(){
     const navigation = useNavigation();
     const dispatch = useDispatch();
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisibleSettings, setModalVisibleSettings] = useState(false);
+    const [modalVisibleEditName, setModalVisibleEditName] = useState(false);
+    const [name, onChangeName] = React.useState("");
+    const [editList, setEditList] = useState('');
 
     const lists = useSelector((state: RootState) => state.listStore.lists)
-    
+    const getUsers = () => {
+        return ['KnQYLRxwzMXrO6ZHlKW9QvHtZzg2']
+    }
     return(
-        <SafeAreaView style={styles.listContainer}>
+        <SafeAreaView>
             <FlatList
                 data={lists}
                 renderItem={({ item }) => (
                     <TouchableHighlight onPress={() => {
                         navigation.navigate('List', { id: item.id })
+                    }}
+                    onLongPress={() => {
+                        setEditList(item.id)
+                        onChangeName(item.name)
+                        setModalVisibleSettings(!modalVisibleSettings)
                     }}>
                         <View style={styles.listContainer}>
                             <Icon style={styles.item} name='rowing' />
                             <Text style={styles.item}>{item.name}</Text>
-                            <Button title=' X ' onPress={()=>{dispatch(removeList(item.id))}}></Button>
                         </View>
                     </TouchableHighlight>
                 )}
             />
-            <Button title='new list' onPress={()=>{dispatch(newList(exampleListOptions))}}></Button>
+            <TouchableOpacity style={styles.buttonSet}>
+                <Button title='Stwórz nową listę' onPress={()=>{setModalVisible(true)}}></Button>
+            </TouchableOpacity>
+            <Modal // Modal dodawania listy
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+                }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <SafeAreaView>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeName}
+                                value={name}
+                                placeholder="Nazwa"
+                            />
+                        </SafeAreaView>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            dispatch(newList({name: name, users: getUsers()}));
+                            setModalVisible(!modalVisible);
+                        }}
+                        >
+                        <Text style={styles.textStyle}>Stwórz listę</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                        }}
+                        >
+                        <Text style={styles.textStyle}>Anuluj</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Modal // Modal ustawień listy
+                animationType="slide"
+                transparent={true}
+                visible={modalVisibleSettings}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisibleSettings(!modalVisibleSettings);
+                }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisibleEditName(!modalVisibleEditName);
+                        }}>
+                            <Text style={styles.textStyle}>Zmień nazwę</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            dispatch(removeList(editList))
+                            console.log(editList)
+                            setModalVisibleSettings(!modalVisibleSettings);
+                        }}>
+                            <Text style={styles.textStyle}>Usuń listę</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisibleSettings(!modalVisibleSettings);
+                        }}>
+                            <Text style={styles.textStyle}>Dodaj znajomych</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisibleSettings(!modalVisibleSettings);
+                        }}>
+                            <Text style={styles.textStyle}>Anuluj</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Modal // Modal edycji listy
+                animationType="slide"
+                transparent={true}
+                visible={modalVisibleEditName}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisibleEditName(!modalVisibleEditName);
+                }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <SafeAreaView>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeName}
+                                value={name}
+                                placeholder="Nazwa"
+                            />
+                        </SafeAreaView>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            dispatch(renameList({id: editList, name: name}))
+                            setModalVisibleEditName(!modalVisibleEditName);
+                        }}>
+                            <Text style={styles.textStyle}>Edytuj listę</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisibleEditName(!modalVisibleEditName);
+                        }}>
+                            <Text style={styles.textStyle}>Anuluj</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
         </SafeAreaView>
     );
 }
--- ZakupexReact/src/app/containers/listScreen.tsx
+++ ZakupexReact/src/app/containers/listScreen.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { View, Text, TouchableHighlight, SafeAreaView, FlatList, Button } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, SafeAreaView, TextInput, View, Text, StyleSheet, TouchableHighlight, Modal, FlatList, Pressable, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem, ListItem } from '../redux/listReducer';
+import { addItem, ListItem, removeItem, modifyItem } from '../redux/listReducer';
 import { RootState } from '../redux/store';
 import styles from '../styles/style'
 
 const exampleItem : ListItem = {
     id: '',
     name: 'item',
-    quantity: 1,
+    quantity: '1',
     icon: ''
 }
 
 export default function ListScreen({ route }) {
     const dispatch = useDispatch();
-    
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisibleEdit, setModalVisibleEdit] = useState(false);
+    const [name, onChangeName] = React.useState("");
+    const [quantity, onChangeQuantity] = React.useState("");
     const items = useSelector((state: RootState) => state.listStore.lists.find((el) => el.id == route.params.id)?.items);
+
+    const [editItem, setEditItem] = useState('');
+
 
     return (
         <View style={{
@@ -26,14 +32,123 @@ export default function ListScreen({ route }) {
             <FlatList
                 data={items}
                 renderItem={({ item }) => (
-                    <TouchableHighlight>
+                    <TouchableHighlight 
+                    onPress={() => {
+                        console.log(item.id)
+                        setEditItem(item.id)
+                        onChangeName(item.name)
+                        onChangeQuantity(item.quantity)
+                        setModalVisibleEdit(!modalVisibleEdit)
+                    }}
+                    onLongPress={() => {
+                        console.log('Long Press: ', item.id)
+                        dispatch(removeItem({listid: route.params.id, itemid: item.id}))
+                    }}>
                         <View style={styles.listContainer}>
                             <Text style={styles.item}>{item.name}</Text>
+                            <Text style={styles.item}>{item.quantity}</Text>
                         </View>
                     </TouchableHighlight>
                 )}
             />
-            <Button title='Add item' onPress={()=>{dispatch(addItem({listid:route.params.id, item: exampleItem}))}}></Button>
+            <Button title='Add item' onPress={() => {
+                onChangeName("")
+                onChangeQuantity("")
+                setModalVisible(true);
+                }}></Button>
+            <Modal  //add item
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisible(!modalVisible);
+                }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <SafeAreaView>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeName}
+                                value={name}
+                                placeholder="Nazwa"
+                            />
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeQuantity}
+                                value={quantity}
+                                placeholder="Ilość"
+                                keyboardType="default"
+                            />
+                        </SafeAreaView>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            dispatch(addItem({listid:route.params.id, item: {id:'', name: name, quantity: quantity, icon: ''}}));
+                            setModalVisible(!modalVisible);
+                        }}
+                        >
+                            <Text style={styles.textStyle}>Dodaj produkt</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisible(!modalVisible);
+                        }}
+                        >
+                            <Text style={styles.textStyle}>Anuluj</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Modal  //edit item
+                animationType="slide"
+                transparent={true}
+                visible={modalVisibleEdit}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setModalVisibleEdit(!modalVisibleEdit);
+                }}
+                >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <SafeAreaView>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeName}
+                                value={name}
+                                placeholder="Nazwa"
+                            />
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangeQuantity}
+                                value={quantity}
+                                placeholder="Ilość"
+                                keyboardType="default"
+                            />
+                        </SafeAreaView>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            dispatch(modifyItem({listid:route.params.id, item: {id:editItem, name: name, quantity: quantity, icon: ''}}));
+                            setModalVisibleEdit(!modalVisibleEdit);
+                        }}
+                        >
+                            <Text style={styles.textStyle}>Edytuj produkt</Text>
+                        </Pressable>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                            setModalVisibleEdit(!modalVisibleEdit);
+                        }}
+                        >
+                            <Text style={styles.textStyle}>Anuluj</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+
         </View>
     );
 };
